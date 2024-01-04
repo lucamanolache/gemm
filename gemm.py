cad650c8c3d64e27d0670e9d07dd5f15843439b1
Luca Manolache <luca1230@gmail.com>
2024-01-03 16:23:13 -0800

    Python

---
+++ gemm.py
@@ -0,0 +1,32 @@
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
+
+import numpy as np
+import time
+
+
+N = 2048
+A = np.random.randn(N, N).astype(dtype=np.float32)
+B = np.random.randn(N, N).astype(dtype=np.float32)
+
+start = time.time()
+C = A @ B
+end = time.time()
+
+C = C.astype(dtype=np.float32)
+
+A = A.flatten()
+B = B.flatten()
+C = C.flatten()
+
+with open("gemm-out", "wb") as f:
+    f.write(A)
+    f.write(B)
+    f.write(C)
+
+print(A[0], A[1], A[-1])
+print(B[0], B[1], B[-1])
+print(C[0], C[1], C[-1])
+
+print((2 * N ** 3) / 1.0e9 / (end - start), "GFLOPS")
